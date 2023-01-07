fdba51b254057b26909bc49ef39d64bcc0198668
ButrintHajrizi <109514384+butrinthajrizi@users.noreply.github.com>
2023-01-08 00:45:58 +0100

    created some files

--- project-attributy-frontend/src/routes/MainPage.tsx
+++ project-attributy-frontend/src/routes/MainPage.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import {Mission} from '../help/types'
-import {getDate} from '../help/funcs'
+import {Mission} from '../types'
  
 function Home() {
   const [missions, setMissions] = useState<Mission[]>([])
@@ -21,7 +20,6 @@ function Home() {
           <img className='mission-image' src={mission.links.mission_patch} width={150}/>
           <h5 className='mission-name'>{mission.mission_name.length !== 0 ? mission.mission_name : "Unknown mission name"}</h5>
           <p className='mission-description'><b>Description: </b>{mission.details === null ? <b className='no-description'>No description provided!</b> : mission.details}</p>
-          <p className='mission-time'><b>Launch date: </b>{getDate(mission.launch_date_utc)}</p>
         </li>
         )}
       </ul>
