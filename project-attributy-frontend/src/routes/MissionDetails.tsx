35c9d3ca47ed100a67a4e2028fcb0a0ed873bfb2
ButrintHajrizi <109514384+butrinthajrizi@users.noreply.github.com>
2023-01-08 16:40:15 +0100

    created a form

--- project-attributy-frontend/src/routes/MissionDetails.tsx
+++ project-attributy-frontend/src/routes/MissionDetails.tsx
@@ -34,15 +34,29 @@ export default function MissionDetails() {
   return (
     <main className='main-details'>
       <aside className='left-side'>
-          <h1>{mission?.mission_name}</h1>
+          <h1 style={{color: "lightblue", fontSize: "2rem", fontWeight: "bold"}}>{mission?.mission_name}</h1>
           <img src={mission?.links.mission_patch} width="500px"/>
-          <h4>{mission?.launch_site.site_name_long}</h4>
-          <p>{mission?.rocket.rocket_name}</p>
-          <p>{mission?.details}</p>
+          <h4 style={style.color}><b style={style.indication}>Site Name: </b>{mission?.launch_site.site_name_long}</h4>
+          <p style={style.color}><b style={style.indication}>Rocket Name: </b>{mission?.rocket.rocket_name}</p>
+          <p style={style.color}><b style={style.indication}>Mission Details: </b>{mission?.details}</p>
       </aside>
       <aside className='right-side'>
-
+        <form>
+          <input type="text" name="name" placeholder='Your name...' required/>
+          <textarea name="comment" placeholder='Leave your comment...' required rows={5}/>
+          <button>Submit</button>
+        </form>
       </aside>
     </main>
   )
+}
+
+const style = {
+  color: {
+    color: "white"
+  },
+  indication: {
+    color: "lightblue",
+    fontSize: "1.1rem"
+  }
 }
